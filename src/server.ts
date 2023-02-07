48b9f0a567cc48499ac87a106f01bfa1836387a2
Nikki Nikkhoui <nnikkhoui@gmail.com>
2023-02-07 14:50:08 -0500

    CAPP-35, CAPP-36 : server, swc, typescript up and running (#1)
    * first push to get server, swc, typescript up and running
    
    * pin deps
    
    Co-authored-by: Chris Wegrzyn <chris.wegrzyn@gmail.com>
    
    * update package lock
    
    ---------
    
    Co-authored-by: Chris Wegrzyn <chris.wegrzyn@gmail.com>

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from './app.js';
+
+const port = app.get('port');
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`server running at http://localhost:${port}`);
+});
