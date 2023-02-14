5ab596fe97a4f02c393fac359f1c2d8714180a44
Nikki Nikkhoui <nnikkhoui@gmail.com>
2023-02-14 16:31:07 -0500

    CAPP-55 Add spec and swagger (#8)
    * add spec and swagger
    
    * remove helath endpoint we dont have that yet
    
    * Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,6 @@
 import express, { Application } from 'express';
+import * as swaggerUi from 'swagger-ui-express';
+import spec from '@App/resources/spec.json' assert { type: 'json' };
 
 const router = express.Router();
 const app: Application = express();
@@ -7,6 +9,12 @@ const app: Application = express();
  * Sets the app to use router and auth
  */
 app.use(router);
+
+/**
+ * Swagger UI documentation endpoint
+ */
+ router.use('/docs', swaggerUi.serve);
+ router.get('/docs', swaggerUi.setup(spec));
 
 app.set('port', process.env.PORT);
 
