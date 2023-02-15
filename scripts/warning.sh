5cbcc298fcd5270840191f7b956dc53121824679
Nikki Nikkhoui <nnikkhoui@gmail.com>
2023-02-15 14:07:19 -0500

    use pnpm

---
+++ scripts/warning.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "##################################################################"
+echo "### IMPORTANT  ###################################################"
+echo "### \"pnpm install\" should only be run inside the container  #####"
+echo "### Please be sure you used \"pnpm container-install\"    #####"
+echo "##################################################################"
