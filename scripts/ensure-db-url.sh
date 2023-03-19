0a01b9319f0b4e57404e09c2645fa95a7b677e3e
Nikki Nikkhoui <nnikkhoui@gmail.com>
2023-03-19 18:07:22 -0400

    fix db scripts

--- scripts/ensure-db-url.sh
+++ scripts/ensure-db-url.sh
@@ -2,7 +2,7 @@
 
 if [ -z "${DATABASE_URL:-}" ]; then
   echo 2>&1 "Database URL is unset. Attempting to set from secret env var..."
-  DATABASE_URL="$(node scripts/get-database-url.js)"
+  DATABASE_URL="$(node scripts/get-db-url.js)"
   if [ -z "${DATABASE_URL:-}" ]; then
     echo 2>&1 "Unable to set URL from secret. Aborting..."
     exit 1
