d946a9f28a2cd1a625c3fc396d95d30ce9851133
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-06 12:16:53 -0400

    added sentry monitoring for errors and performance

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
+import * as Sentry from '@sentry/node';
 import * as swaggerUi from 'swagger-ui-express';
 import spec from '@App/resources/spec.json' assert { type: 'json' };
 import {
@@ -11,6 +12,33 @@ import AuthService from './services/AuthService.js';
 
 const getApp = (authService: AuthService): Application => {
   const app: Application = express();
+
+  /**
+   * Initialize Sentry
+   */
+  Sentry.init({
+    dsn: 'https://c38ab9f98fd0404f9d2bfb95d015da8d@o4504962952724480.ingest.sentry.io/4504963428777984',
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Sentry.Integrations.Express({ app }),
+      // Automatically instrument Node.js libraries and frameworks
+      ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+
+  // RequestHandler creates a separate execution context using domains, so that every
+  // transaction/span/breadcrumb is attached to its own Hub instance
+  app.use(Sentry.Handlers.requestHandler());
+  // TracingHandler creates a trace for every incoming request
+  app.use(Sentry.Handlers.tracingHandler());
+
   const router = express.Router();
   app.use(express.json());
   /**
@@ -27,6 +55,14 @@ const getApp = (authService: AuthService): Application => {
   router.get('/docs', swaggerUi.setup(spec));
 
   router.get('/health', healthRoutes());
+
+  // for testing error capturing in Sentry
+  router.get('/debug-sentry', () => {
+    throw new Error('Debug Sentry error!');
+  });
+
+  // The error handler must be before any other error middleware and after all controllers
+  app.use(Sentry.Handlers.errorHandler());
 
   app.use(errorHandler);
   app.set('port', process.env.PORT);
