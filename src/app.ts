9a52463362eaa5b564f1989f7295eb17316fee47
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-06 18:14:11 -0400

    commenting out sentry code in app

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import express, { Application } from 'express';
-import * as Sentry from '@sentry/node';
+// import * as Sentry from '@sentry/node';
 import * as swaggerUi from 'swagger-ui-express';
 import spec from '@App/resources/spec.json' assert { type: 'json' };
 import {
@@ -16,28 +16,28 @@ const getApp = (authService: AuthService): Application => {
   /**
    * Initialize Sentry
    */
-  Sentry.init({
-    dsn: 'https://c38ab9f98fd0404f9d2bfb95d015da8d@o4504962952724480.ingest.sentry.io/4504963428777984',
-    integrations: [
-      // enable HTTP calls tracing
-      new Sentry.Integrations.Http({ tracing: true }),
-      // enable Express.js middleware tracing
-      new Sentry.Integrations.Express({ app }),
-      // Automatically instrument Node.js libraries and frameworks
-      ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
-    ],
+  // Sentry.init({
+  //   dsn: 'https://c38ab9f98fd0404f9d2bfb95d015da8d@o4504962952724480.ingest.sentry.io/4504963428777984',
+  //   integrations: [
+  //     // enable HTTP calls tracing
+  //     new Sentry.Integrations.Http({ tracing: true }),
+  //     // enable Express.js middleware tracing
+  //     new Sentry.Integrations.Express({ app }),
+  //     // Automatically instrument Node.js libraries and frameworks
+  //     ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+  //   ],
 
-    // Set tracesSampleRate to 1.0 to capture 100%
-    // of transactions for performance monitoring.
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
-  });
+  //   // Set tracesSampleRate to 1.0 to capture 100%
+  //   // of transactions for performance monitoring.
+  //   // We recommend adjusting this value in production
+  //   tracesSampleRate: 1.0,
+  // });
 
-  // RequestHandler creates a separate execution context using domains, so that every
-  // transaction/span/breadcrumb is attached to its own Hub instance
-  app.use(Sentry.Handlers.requestHandler());
-  // TracingHandler creates a trace for every incoming request
-  app.use(Sentry.Handlers.tracingHandler());
+  // // RequestHandler creates a separate execution context using domains, so that every
+  // // transaction/span/breadcrumb is attached to its own Hub instance
+  // app.use(Sentry.Handlers.requestHandler());
+  // // TracingHandler creates a trace for every incoming request
+  // app.use(Sentry.Handlers.tracingHandler());
 
   const router = express.Router();
   app.use(express.json());
@@ -62,7 +62,7 @@ const getApp = (authService: AuthService): Application => {
   });
 
   // The error handler must be before any other error middleware and after all controllers
-  app.use(Sentry.Handlers.errorHandler());
+  // app.use(Sentry.Handlers.errorHandler());
 
   app.use(errorHandler);
   app.set('port', process.env.PORT);
