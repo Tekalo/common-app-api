be7f09314c073e9e6eb56a966124448dda4b9c06
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-06 15:02:53 -0400

    made sentry dsn an environment variable

--- tf/envs/dev/main.tf
+++ tf/envs/dev/main.tf
@@ -49,6 +49,7 @@ module "app" {
   image                = var.image
   cli_image            = var.cli_image
   auth0_domain         = var.auth0_domain
+  sentry_dsn           = var.sentry_dsn
 
   rotation_vpc_security_group_id = module.envconfig.database_ingress_security_group_id
   rotation_vpc_subnet_ids        = module.envconfig.private_subnet_ids
