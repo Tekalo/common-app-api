fdeb3488d9d525bf86a8ec6d46849a80fe44a850
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-07 18:51:48 -0400

    sleep for longer for github actions

--- src/tests/integration/monitoringService.test.ts
+++ src/tests/integration/monitoringService.test.ts
@@ -38,7 +38,7 @@ describe('Error Handling', () => {
       })
       .expect(500);
 
-    await sleep(100);
+    await sleep(200);
 
     expect(testkit.transactions()).toHaveLength(1);
     expect(testkit.reports()).toHaveLength(1);
@@ -54,7 +54,7 @@ describe('Error Handling', () => {
 
     await request(dummyAuthApp).get('/health').expect(200);
 
-    await sleep(100);
+    await sleep(200);
 
     expect(testkit.transactions()).toHaveLength(2);
     const transaction = testkit.transactions()[1];
