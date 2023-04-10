6f994c73b6ae5931eaffc7dfb72d8991592b7591
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-10 13:16:33 -0400

    add SENTRY_DSN to task environment; add env to configLoader; pass env to Sentry

--- src/services/configLoader.ts
+++ src/services/configLoader.ts
@@ -3,6 +3,7 @@ import { Auth0Config } from '@App/resources/types/auth0.js';
 import { Auth0ConfigSchema } from '@App/resources/schemas/auth0.js';
 
 export type BaseConfig = {
+  env: string;
   port: number;
   auth0: Auth0Config;
   sentryDSN: string;
@@ -18,6 +19,7 @@ function loadConfig(): BaseConfig {
     JSON.parse(process.env.AUTH0_CONFIG),
   );
   const configObj = {
+    env: process.env.APP_ENV || 'dev',
     port: Number(process.env.PORT) || 3000,
     auth0: validatedAuth0Config,
     sentryDSN: process.env.SENTRY_DSN || '',
