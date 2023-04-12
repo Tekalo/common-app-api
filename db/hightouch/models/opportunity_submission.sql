d6a2a1389b821b9a836b52f02133a24ed3c1fccf
Logan Gittelson <lgittelson@schmidtfutures.com>
2023-04-12 14:56:17 -0400

    Updating opportunities

--- db/hightouch/models/opportunity_submission.sql
+++ db/hightouch/models/opportunity_submission.sql
@@ -6,7 +6,32 @@ SELECT
   ob."orgName",
   ob."orgSize",
   ob."orgType",
-  os.*
+  ob."equalOpportunityEmployer",
+  ob."orgName" || ' - ' || os."positionTitle" AS "opportunityName",
+  os.id,
+  os."opportunityBatchId",
+  os."createdAt",
+  os."source",
+  os."employmentType",
+  CASE
+    WHEN os.paid THEN 'Paid'
+    ELSE 'Unpaid'
+  END AS "paymentStatus",
+  os."roleType",
+  os."salaryRange",
+  os."positionTitle",
+  os."fullyRemote",
+  os."location",
+  os."visaSponsorship",
+  os."desiredHoursPerWeek",
+  os."desiredStartDate",
+  os."desiredEndDate",
+  os."desiredYoe",
+  ARRAY_CAT(os."desiredSkills", os."desiredSkillsOther") AS "desiredSkillsAll",
+  os."desiredImpactExp",
+  os."similarStaffed",
+  os."jdUrl",
+  os."pitchEssay"
 FROM
   "OpportunitySubmission" os
   LEFT JOIN "OpportunityBatch" ob ON os."opportunityBatchId" = ob.id;
