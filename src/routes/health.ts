413ff5d23908d60c545fc5d4f9015bbb58c488dd
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-24 16:21:37 -0400

    update health check to return 503 if the app is in fact not healthy

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -2,8 +2,15 @@ import express, { Request, Response } from 'express';
 
 const healthRoutes = () => {
   const router = express.Router();
-  router.get('/', (req: Request, res: Response) => {
-    res.status(200).send({ healthy: true });
+  router.get('/', (req: Request, res: Response, next) => {
+    const healthcheck = {
+      healthy: true,
+    };
+    try {
+      res.status(200).send(healthcheck);
+    } catch (error) {
+      res.status(503).send();
+    }
   });
   return router;
 };
