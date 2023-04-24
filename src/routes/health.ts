a0754531f80010b85c46e888c74ba3290dbf88df
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-04-24 17:13:18 -0400

    added a HealthService that checks a database query

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,16 +1,18 @@
 import express, { Request, Response } from 'express';
+import HealthService, { ResourceHealth } from '@App/services/HealthService.js';
 
 const healthRoutes = () => {
   const router = express.Router();
   router.get('/', (req: Request, res: Response, next) => {
-    const healthcheck = {
-      healthy: true,
-    };
-    try {
-      res.status(200).send(healthcheck);
-    } catch (error) {
-      res.status(503).send();
-    }
+    const healthService = new HealthService();
+    healthService
+      .getHealth()
+      .then((result) => {
+        res.status(result.status === ResourceHealth.Healthy ? 200 : 503).send({
+          healthy: result.status === ResourceHealth.Healthy,
+        });
+      })
+      .catch((err) => next(err));
   });
   return router;
 };
