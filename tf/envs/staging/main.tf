dfe9c5ddb4af0c492b96d0e6967fd82863b2ceb7
Chris Wegrzyn <cwegrzyn@schmidtfutures.com>
2023-04-27 11:58:16 -0400

    terraform fmt

--- tf/envs/staging/main.tf
+++ tf/envs/staging/main.tf
@@ -63,34 +63,34 @@ module "env_defns" {
 module "autoscaling" {
   source = "../../modules/autoscale"
 
-  env                 = module.envconfig.env
-  ecs_cluster         = module.envconfig.ecs_cluster
-  service_name        = module.app.service_name
+  env          = module.envconfig.env
+  ecs_cluster  = module.envconfig.ecs_cluster
+  service_name = module.app.service_name
 
-  min_capacity        = 2
-  max_capacity        = 10
+  min_capacity = 2
+  max_capacity = 10
 
   metrics = {
     CPUAverage = {
-      target            = 60
+      target = 60
       predefined_metric = [{
         type = "ECSServiceAverageCPUUtilization"
       }]
     }
     MemoryAverage = {
-      target            = 60
+      target = 60
       predefined_metric = [{
         type = "ECSServiceAverageMemoryUtilization"
       }]
     }
     CPUSpike = {
-      target            = 85
+      target = 85
       customized_metric = [{
-        metric_name     = "CPUUtilization"
-        namespace       = "AWS/ECS"
-        statistic       = "Maximum"
-        unit            = "Percent"
-        dimensions   = {
+        metric_name = "CPUUtilization"
+        namespace   = "AWS/ECS"
+        statistic   = "Maximum"
+        unit        = "Percent"
+        dimensions = {
           "ClusterName" = module.envconfig.ecs_cluster
           "ServiceName" = module.app.service_name
         }
