26a1516e67ff7fc6b836b1c476fb826ba337ad90
Logan Gittelson <lgittelson@schmidtfutures.com>
2023-05-04 14:21:27 -0400

    Adding note about where to find password

--- db/resources/ht_read_only_user.sql
+++ db/resources/ht_read_only_user.sql
@@ -1,3 +1,5 @@
+-- Note: Password is in AWS Secrets Manager
+
 CREATE ROLE ht_read_only NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'xxxxx';
 GRANT USAGE ON SCHEMA public TO ht_read_only;
 GRANT SELECT ON TABLE public."Applicant" TO ht_read_only;
