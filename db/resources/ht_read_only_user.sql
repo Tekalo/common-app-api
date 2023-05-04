0844a94f326679685b68f2652df41161c67d79b9
Logan Gittelson <lgittelson@schmidtfutures.com>
2023-05-04 14:20:07 -0400

    Logging sql used to create the HT read only user

---
+++ db/resources/ht_read_only_user.sql
@@ -0,0 +1,7 @@
+CREATE ROLE ht_read_only NOSUPERUSER NOCREATEDB NOCREATEROLE NOINHERIT LOGIN NOREPLICATION NOBYPASSRLS PASSWORD 'xxxxx';
+GRANT USAGE ON SCHEMA public TO ht_read_only;
+GRANT SELECT ON TABLE public."Applicant" TO ht_read_only;
+GRANT SELECT ON TABLE public."ApplicantDeletionRequests" TO ht_read_only;
+GRANT SELECT ON TABLE public."ApplicantSubmission" TO ht_read_only;
+GRANT SELECT ON TABLE public."OpportunityBatch" TO ht_read_only;
+GRANT SELECT ON TABLE public."OpportunitySubmission" TO ht_read_only;
