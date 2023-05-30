5030a31a847b3821dbe1365f449732babb6033cd
Nikki Nikkhoui <nnikkhoui@schmidtfutures.com>
2023-05-30 12:05:45 -0400

    rds version (#262)

--- tf/modules/app/main.tf
+++ tf/modules/app/main.tf
@@ -23,7 +23,9 @@ resource "aws_rds_cluster" "main" {
 
   engine                 = "aurora-postgresql"
   engine_mode            = "provisioned"
-  engine_version         = "14"
+   # This only exists as a temporary solution until CAPP-690 is resolved
+   # We want to ideally have version 15 in all envs
+  engine_version         = var.env == "dev" ? 15 : 14
   database_name          = "capp"
   master_username        = var.db_username
   master_password        = var.db_password
