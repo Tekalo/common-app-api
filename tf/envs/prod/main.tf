0a61c79e2a07c6b80d993b39ed11b6236de256a5
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-05-31 14:05:32 -0400

    add alarms_enabled to app

--- tf/envs/prod/main.tf
+++ tf/envs/prod/main.tf
@@ -59,6 +59,7 @@ module "app" {
   notify_webhook            = var.notify_webhook
   slack_channel             = var.slack_channel
   slack_username            = var.slack_username
+  alarms_enabled            = var.alarms_enabled
 
   rotation_vpc_security_group_id = module.envconfig.database_ingress_security_group_id
   rotation_vpc_subnet_ids        = module.envconfig.private_subnet_ids
