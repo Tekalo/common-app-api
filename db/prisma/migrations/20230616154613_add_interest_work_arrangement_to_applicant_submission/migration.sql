98f946373bea9779411b9a27d34b77cd4b1fe48b
Nikki Nikkhoui <nnikkhoui@gmail.com>
2023-06-16 12:08:36 -0400

    update seed.json, add migration

---
+++ db/prisma/migrations/20230616154613_add_interest_work_arrangement_to_applicant_submission/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "ApplicantDraftSubmission" ADD COLUMN     "interestWorkArrangement" TEXT;
+
+-- AlterTable
+ALTER TABLE "ApplicantSubmission" ADD COLUMN     "interestWorkArrangement" TEXT;
