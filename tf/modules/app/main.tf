a7f939a4c924560c2660143c3e89f3c3592f8a7c
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-06-16 11:51:35 -0400

    update retention period

--- tf/modules/app/main.tf
+++ tf/modules/app/main.tf
@@ -50,7 +50,7 @@ resource "aws_rds_cluster" "main" {
 
   enabled_cloudwatch_logs_exports = [ "postgresql" ]
   create_cloudwatch_log_group     = true
-  cloudwatch_log_group_retention_in_days = 30
+  cloudwatch_log_group_retention_in_days = 7
 }
 
 resource "aws_rds_cluster_instance" "instance0" {
