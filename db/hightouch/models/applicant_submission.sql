9bf3c7601dabd879ac78beeeec805631ab9bdb8c
Logan Gittelson <lgittelson@schmidtfutures.com>
2023-06-26 15:22:12 -0400

    Reverting changes to openToRemote

--- db/hightouch/models/applicant_submission.sql
+++ db/hightouch/models/applicant_submission.sql
@@ -57,7 +57,14 @@ SELECT
   ) AS "allSkills",
   appsub."currentLocation",
   appsub."openToRelocate",
-  appsub."openToRemote",
+  CASE
+    appsub."openToRemote"
+    WHEN 'only remote' THEN 'Only open to remote'
+    WHEN 'no remote' THEN 'Not open to remote'
+    WHEN 'both' THEN 'Open to in-person or remote'
+    WHEN 'not sure' THEN 'Not Sure'
+    ELSE appsub."openToRemote"
+  END "openToRemote",
   appsub."desiredSalary",
   appsub."previousImpactExperience",
   CASE
