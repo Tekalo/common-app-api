cbbc61590c1c7e1d8ebf10fa564eab9b2e420267
Nikki Nikkhoui <nnikkhoui@gmail.com>
2023-07-17 07:56:54 -0400

    match FE zod schema for url

--- src/resources/schemas/applicants.ts
+++ src/resources/schemas/applicants.ts
@@ -77,7 +77,7 @@ const ApplicantSubmissionRequestBodySchema = z.object({
   githubUrl: z.string().max(500).nullable().optional(),
   portfolioUrl: z.string().max(500).nullable().optional(),
   portfolioPassword: z.string().max(255).nullable().optional(),
-  resumeUrl: z.string().max(500).optional(),
+  resumeUrl: z.string().url().optional(),
   resumePassword: z.string().max(255).nullable().optional(),
   hoursPerWeek: z.string().max(255).nullable().optional(),
   interestEmploymentType: z.array(EmploymentType),
