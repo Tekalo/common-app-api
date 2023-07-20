0e2e8500061638ea39ad6af9bdd4ddea80627b19
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-07-20 10:36:59 -0400

    updated prisma migration

--- db/prisma/migrations/20230720143603_add_uploads_table/migration.sql
+++ db/prisma/migrations/20230720143603_add_uploads_table/migration.sql
@@ -2,17 +2,17 @@
 CREATE TYPE "UploadType" AS ENUM ('RESUME');
 
 -- CreateEnum
-CREATE TYPE "UploadStatus" AS ENUM ('SUCCESS', 'FAILED');
+CREATE TYPE "UploadStatus" AS ENUM ('REQUESTED', 'SUCCESS', 'FAILURE');
 
 -- CreateTable
 CREATE TABLE "Uploads" (
-    "id" TEXT NOT NULL,
+    "id" SERIAL NOT NULL,
     "applicantId" INTEGER NOT NULL,
     "type" "UploadType" NOT NULL,
     "originalFilename" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "completedAt" TIMESTAMP(3),
-    "status" "UploadStatus",
+    "status" "UploadStatus" DEFAULT 'REQUESTED',
 
     CONSTRAINT "Uploads_pkey" PRIMARY KEY ("id")
 );
