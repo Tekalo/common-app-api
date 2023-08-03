e05f84a9b56b6bc01956661c4ef9f8a3422f5444
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-08-03 08:29:30 -0700

    removed accidental check-in of tf build file; update DummyS3Service.generateSignedUploadUrl to include contentType

--- src/tests/fixtures/DummyS3Service.ts
+++ src/tests/fixtures/DummyS3Service.ts
@@ -2,8 +2,12 @@
 import S3Service from '@App/services/S3Service.js';
 
 class DummyS3Service extends S3Service {
-  async generateSignedUploadUrl(bucket: string, key: string) {
-    const url = `https://${bucket}.s3.us-east-1.amazonaws.com/${key}?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=XXXX%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230724T211311Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&x-id=PutObject`;
+  async generateSignedUploadUrl(
+    bucket: string,
+    key: string,
+    contentType: string,
+  ) {
+    const url = `https://${bucket}.s3.us-east-1.amazonaws.com/${key}?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&Content-Type=${contentType}&X-Amz-Credential=XXXX%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230724T211311Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&x-id=PutObject`;
     return Promise.resolve(url);
   }
 }
