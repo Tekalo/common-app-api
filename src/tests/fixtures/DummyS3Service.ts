2273c043c2f73ca8691c12832a024c43241a94f2
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-08-09 08:04:35 -0700

    delete resume uploads for an applicant when they've requested account deletion (or we're deleting a test account)

--- src/tests/fixtures/DummyS3Service.ts
+++ src/tests/fixtures/DummyS3Service.ts
@@ -10,6 +10,11 @@ class DummyS3Service extends S3Service {
     const url = `https://${bucket}.s3.us-east-1.amazonaws.com/${key}?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&Content-Type=${contentType}&X-Amz-Credential=XXXX%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20230724T211311Z&X-Amz-Expires=900&X-Amz-SignedHeaders=host&x-id=PutObject`;
     return Promise.resolve(url);
   }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async deleteUploads(bucket: string, prefix: string): Promise<void> {
+    return Promise.resolve();
+  }
 }
 
 export default DummyS3Service;
