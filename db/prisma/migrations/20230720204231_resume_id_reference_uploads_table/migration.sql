40d885dd75206ef382dd8d8fd760cd122284bfc4
Nikki Nikkhoui <nnikkhoui@gmail.com>
2023-08-21 08:28:20 -0400

    dont do a find and replace all when tired

--- db/prisma/migrations/20230720204231_resume_id_reference_uploads_table/migration.sql
+++ db/prisma/migrations/20230720204231_resume_id_reference_uploads_table/migration.sql
@@ -1,23 +1,23 @@
 /*
   Warnings:
 
-  - The `resumeId` column on the `ApplicantDraftSubmission` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-  - The `resumeId` column on the `ApplicantSubmission` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `resumeUploadId` column on the `ApplicantDraftSubmission` table would be dropped and recreated. This will lead to data loss if there is data in the column.
+  - The `resumeUploadId` column on the `ApplicantSubmission` table would be dropped and recreated. This will lead to data loss if there is data in the column.
   - You are about to drop the `Uploads` table. If the table is not empty, all the data it contains will be lost.
-  - A unique constraint covering the columns `[resumeId]` on the table `ApplicantDraftSubmission` will be added. If there are existing duplicate values, this will fail.
-  - A unique constraint covering the columns `[resumeId]` on the table `ApplicantSubmission` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[resumeUploadId]` on the table `ApplicantDraftSubmission` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[resumeUploadId]` on the table `ApplicantSubmission` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- DropForeignKey
 ALTER TABLE "Uploads" DROP CONSTRAINT "Uploads_applicantId_fkey";
 
 -- AlterTable
-ALTER TABLE "ApplicantDraftSubmission" DROP COLUMN "resumeId",
-ADD COLUMN     "resumeId" INTEGER;
+ALTER TABLE "ApplicantDraftSubmission" DROP COLUMN "resumeUploadId",
+ADD COLUMN     "resumeUploadId" INTEGER;
 
 -- AlterTable
-ALTER TABLE "ApplicantSubmission" DROP COLUMN "resumeId",
-ADD COLUMN     "resumeId" INTEGER;
+ALTER TABLE "ApplicantSubmission" DROP COLUMN "resumeUploadId",
+ADD COLUMN     "resumeUploadId" INTEGER;
 
 -- DropTable
 DROP TABLE "Uploads";
@@ -36,16 +36,16 @@ CREATE TABLE "Upload" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ApplicantDraftSubmission_resumeId_key" ON "ApplicantDraftSubmission"("resumeId");
+CREATE UNIQUE INDEX "ApplicantDraftSubmission_resumeUploadId_key" ON "ApplicantDraftSubmission"("resumeUploadId");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "ApplicantSubmission_resumeId_key" ON "ApplicantSubmission"("resumeId");
+CREATE UNIQUE INDEX "ApplicantSubmission_resumeUploadId_key" ON "ApplicantSubmission"("resumeUploadId");
 
 -- AddForeignKey
-ALTER TABLE "ApplicantSubmission" ADD CONSTRAINT "ApplicantSubmission_resumeId_fkey" FOREIGN KEY ("resumeId") REFERENCES "Upload"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "ApplicantSubmission" ADD CONSTRAINT "ApplicantSubmission_resumeUploadId_fkey" FOREIGN KEY ("resumeUploadId") REFERENCES "Upload"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ApplicantDraftSubmission" ADD CONSTRAINT "ApplicantDraftSubmission_resumeId_fkey" FOREIGN KEY ("resumeId") REFERENCES "Upload"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "ApplicantDraftSubmission" ADD CONSTRAINT "ApplicantDraftSubmission_resumeUploadId_fkey" FOREIGN KEY ("resumeUploadId") REFERENCES "Upload"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Upload" ADD CONSTRAINT "Upload_applicantId_fkey" FOREIGN KEY ("applicantId") REFERENCES "Applicant"("id") ON DELETE CASCADE ON UPDATE CASCADE;
