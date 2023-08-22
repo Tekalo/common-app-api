194c0221562ecfd9e5156546c03d4dec41976d48
Nikki Nikkhoui <nnikkhoui@gmail.com>
2023-08-22 09:10:05 -0400

    make schema dir, add empty .ts files for each schema type

---
+++ schemas/index.ts
@@ -0,0 +1,7 @@
+import applicants from './applicants.js';
+import auth0 from './auth0.js';
+import opportunities from './opportunities.js';
+import uploads from './uploads.js';
+import config from './config.js';
+
+export { applicants, auth0, opportunities, config, uploads };
