1ec404439480a72282b0bdded8c12e040537b8a4
Nikki Nikkhoui <nnikkhoui@schmidtfutures.com>
2023-08-23 14:02:12 -0400

    make resumeUpload a required field (#415)

--- src/tests/util/helpers.ts
+++ src/tests/util/helpers.ts
@@ -1,4 +1,6 @@
+import prisma from '@App/resources/client.js';
 import { BaseConfig } from '@App/resources/types/shared.js';
+import { Upload } from '@prisma/client';
 
 /**
  * Prepend this function to tests that you want to conditionally run
@@ -7,6 +9,17 @@ import { BaseConfig } from '@App/resources/types/shared.js';
 const itif = (condition: boolean) => (condition ? it : it.skip);
 
 const getRandomString = () => Math.random().toString(36).slice(2);
+
+const seedResumeUpload = async (applicantId: number): Promise<Upload> =>
+  prisma.upload.create({
+    data: {
+      type: 'RESUME',
+      status: 'SUCCESS',
+      applicantId,
+      originalFilename: 'myresume.pdf',
+      contentType: 'application/pdf',
+    },
+  });
 
 const getMockConfig = (overrides: Partial<BaseConfig> = {}): BaseConfig => ({
   env: '',
@@ -37,4 +50,4 @@ const getMockConfig = (overrides: Partial<BaseConfig> = {}): BaseConfig => ({
   ...overrides,
 });
 
-export { itif, getRandomString, getMockConfig };
+export { itif, getRandomString, getMockConfig, seedResumeUpload };
