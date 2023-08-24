34e6971a6c130acdcbfccd5e63d843373c9fab99
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-08-24 15:02:01 -0400

    remove graceful shutdown server code

--- src/server.ts
+++ src/server.ts
@@ -25,20 +25,7 @@ const app = getApp(
 
 const port = +app.get('port');
 
-const server = app.listen(port, () => {
+app.listen(port, () => {
   // eslint-disable-next-line no-console
   logger.info(`server running at http://localhost:${port}`);
 });
-
-function gracefulShutdown() {
-  logger.info('Shutting down');
-  server.close(() => {
-    logger.info('HTTP server closed.');
-
-    // When server has stopped accepting connections
-    // exit the process with exit status 0
-    process.exit(0);
-  });
-}
-
-process.on('SIGTERM', gracefulShutdown);
