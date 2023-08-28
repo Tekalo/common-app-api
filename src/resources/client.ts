132ee67567498bde9280e47c51afcf5397da59bf
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-08-28 19:27:40 -0400

    listen for error log events and issue a SIGTERM if there is a password auth error

--- src/resources/client.ts
+++ src/resources/client.ts
@@ -1,7 +1,29 @@
 import { PrismaClient } from '@prisma/client';
 import { PrismaSessionStore } from '@quixo3/prisma-session-store';
+import logger from '@App/services/logger.js';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: [
+    {
+      emit: 'event',
+      level: 'error',
+    },
+  ],
+});
+
+prisma.$on('error', (err) => {
+  if (
+    err.message
+      .replaceAll(/`.*?`/g, '')
+      .includes(
+        'Authentication failed against database server at , the provided database credentials for  are not valid.',
+      )
+  ) {
+    logger.error('Password auth failed; shutting down');
+    process.kill(process.pid, 'SIGTERM');
+  }
+});
+
 const sessionStore = new PrismaSessionStore(prisma, {
   checkPeriod: 2 * 60 * 1000, // 2 minutes
 });
