2ba26ef684ec770b3b487c47e2dc37cc2670f711
Nikki Nikkhoui <nnikkhoui@gmail.com>
2023-09-21 09:21:44 -0400

    minor syntax tweaks to custom migration file

--- db/prisma/migrations/20230920195958_add_submission_update_at/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE "ApplicantSubmission" ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT '1970-01-01 00:00:00 +00:00', ALTER COLUMN updatedAt DROP DEFAULT;
-
-UPDATE ApplicantSubmission
-SET updatedAt = createdAt
-WHERE updatedAt = '1970-01-01 00:00:00 +00:00';
