d373cd97122bdf51ccdfefa693d52b3dec2da8c9
Nikki Nikkhoui <nnikkhoui@gmail.com>
2023-09-26 08:58:57 -0400

    string limit

--- packages/schemas/src/shared.ts
+++ packages/schemas/src/shared.ts
@@ -1,15 +1,15 @@
 import { z } from 'zod';
 
 const UTMPayload = z.object({
-  utm_source: z.string().nullish(),
-  utm_medium: z.string().nullish(),
-  utm_campaign: z.string().nullish(),
-  utm_term: z.string().nullish(),
-  utm_content: z.string().nullish(),
-  utm_id: z.string().nullish(),
-  utm_source_platform: z.string().nullish(),
-  ga_client_id: z.string().nullish(),
-  ga_session_id: z.string().nullish(),
+  utm_source: z.string().max(200).nullish(),
+  utm_medium: z.string().max(200).nullish(),
+  utm_campaign: z.string().max(200).nullish(),
+  utm_term: z.string().max(200).nullish(),
+  utm_content: z.string().max(200).nullish(),
+  utm_id: z.string().max(200).nullish(),
+  utm_source_platform: z.string().max(200).nullish(),
+  ga_client_id: z.string().max(200).nullish(),
+  ga_session_id: z.string().max(200).nullish(),
 });
 
 export default UTMPayload;
