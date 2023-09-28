26e058c4773bb6d253b98deb8d2ddfdc592ec622
Nikki Nikkhoui <nnikkhoui@gmail.com>
2023-09-28 16:23:42 -0700

    prisma migration

--- db/prisma/migrations/20230928232304_add_utm_params/migration.sql
+++ db/prisma/migrations/20230928232304_add_utm_params/migration.sql
@@ -2,12 +2,16 @@
   Warnings:
 
   - A unique constraint covering the columns `[utmParamsId]` on the table `Applicant` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[utmParamsId]` on the table `ApplicantDraftSubmission` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[utmParamsId]` on the table `ApplicantSubmission` will be added. If there are existing duplicate values, this will fail.
   - A unique constraint covering the columns `[utmParamsId]` on the table `OpportunityBatch` will be added. If there are existing duplicate values, this will fail.
 
 */
 -- AlterTable
 ALTER TABLE "Applicant" ADD COLUMN     "utmParamsId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "ApplicantDraftSubmission" ADD COLUMN     "utmParamsId" INTEGER;
 
 -- AlterTable
 ALTER TABLE "ApplicantSubmission" ADD COLUMN     "utmParamsId" INTEGER;
@@ -28,6 +32,9 @@ CREATE TABLE "UTMParams" (
 CREATE UNIQUE INDEX "Applicant_utmParamsId_key" ON "Applicant"("utmParamsId");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "ApplicantDraftSubmission_utmParamsId_key" ON "ApplicantDraftSubmission"("utmParamsId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "ApplicantSubmission_utmParamsId_key" ON "ApplicantSubmission"("utmParamsId");
 
 -- CreateIndex
@@ -38,6 +45,9 @@ ALTER TABLE "Applicant" ADD CONSTRAINT "Applicant_utmParamsId_fkey" FOREIGN KEY 
 
 -- AddForeignKey
 ALTER TABLE "ApplicantSubmission" ADD CONSTRAINT "ApplicantSubmission_utmParamsId_fkey" FOREIGN KEY ("utmParamsId") REFERENCES "UTMParams"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ApplicantDraftSubmission" ADD CONSTRAINT "ApplicantDraftSubmission_utmParamsId_fkey" FOREIGN KEY ("utmParamsId") REFERENCES "UTMParams"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "OpportunityBatch" ADD CONSTRAINT "OpportunityBatch_utmParamsId_fkey" FOREIGN KEY ("utmParamsId") REFERENCES "UTMParams"("id") ON DELETE SET NULL ON UPDATE CASCADE;
