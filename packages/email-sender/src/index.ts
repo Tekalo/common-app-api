f9617cc135c397061a677ddc65478bf928660237
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-11-16 10:55:27 -0500

    update to working email-sender

--- packages/email-sender/src/index.ts
+++ packages/email-sender/src/index.ts
@@ -1,19 +1,19 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { SQSEvent, SQSHandler, SQSRecord } from 'aws-lambda';
-import { SendEmailCommandInput } from '@aws-sdk/client-ses';
-import SESService from './SESService.js';
+// import { SendEmailCommandInput } from '@aws-sdk/client-ses';
+// import SESService from './SESService.js';
 
-const sesService = new SESService();
+// const sesService = new SESService();
 
-const processMessage = async (message: SQSRecord) => {
+const processMessage = (message: SQSRecord) => {
   try {
     // eslint-disable-next-line no-console
     console.log(`Processed message ${message.body}`);
-    const parsedMessage: SendEmailCommandInput = JSON.parse(
-      message.body,
-    ) as SendEmailCommandInput;
+    // const parsedMessage: SendEmailCommandInput = JSON.parse(
+    //   message.body,
+    // ) as SendEmailCommandInput;
 
-    return await sesService.sendEmail(parsedMessage);
+    // return await sesService.sendEmail(parsedMessage);
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error('There was a problem');
@@ -21,8 +21,8 @@ const processMessage = async (message: SQSRecord) => {
   }
 };
 
-const handler: SQSHandler = async (event: SQSEvent) => {
-  await processMessage(event.Records[0]);
+export const handler: SQSHandler = (event: SQSEvent) => {
+  processMessage(event.Records[0]);
   // eslint-disable-next-line no-console
   console.info('done');
 };
