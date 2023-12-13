01f0dcf261579154eed714b942d7d2a3c30b2284
Nikki Nikkhoui <nnikkhoui@schmidtfutures.com>
2023-12-13 12:14:49 -0500

    add back otherskills to response body

--- src/tests/unit/controllers/applicants.controller.test.ts
+++ src/tests/unit/controllers/applicants.controller.test.ts
@@ -1123,6 +1123,7 @@ describe('Applicant Controller', () => {
         lastOrg: 'mozilla',
         yoe: '>11',
         skills: ['react', 'python'], // enum
+        otherSkills: [],
         linkedInUrl: 'https://www.linkedin.com/in/bob-bobberson',
         githubUrl: 'https://github.com/bboberson',
         portfolioUrl: null,
