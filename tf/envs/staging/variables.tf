3723722437ce1bf80be4849f48080aec61130cfb
Anna Armentrout <aarmentrout@schmidtfutures.com>
2023-12-20 13:36:07 -0500

    update staging terraform to use a different workspace; update account id

--- tf/envs/staging/variables.tf
+++ tf/envs/staging/variables.tf
@@ -13,3 +13,7 @@ variable "ses_whitelist" {
   description = "Email addresses SES is permitted to send to only in non-prod environments"
   type        = string
 }
+
+variable "bucket_env" {
+  type = string
+}
