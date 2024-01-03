b66c18235a2dbde8eb2d75022b03e324e4ad0522
Logan Gittelson <lgittelson@schmidtfutures.com>
2024-01-03 15:10:21 -0500

    Updating incoming skills sql

--- db/hightouch/models/incoming_skills.sql
+++ db/hightouch/models/incoming_skills.sql
@@ -1,20 +1,8 @@
-SELECT
-  NAME
-FROM
-  "ReferenceSkills"
-WHERE
-  NAME IS NOT NULL
-UNION
-SELECT
-  NAME
-FROM
-  "ApplicantSkills"
-WHERE
-  NAME IS NOT NULL
-UNION
-SELECT
-  NAME
-FROM
-  "OpportunitySkills"
-WHERE
-  NAME IS NOT NULL;
+/**
+ * The hard-coded '2023-12-01' references the day the ReferenceSkills were first loaded into the database
+ */
+select distinct coalesce(rs.name, as2.name, os.name) as name, COALESCE(LEAST(as2."createdAt", os."createdAt"), '2023-12-01'::date) as "createdAt"
+from "ReferenceSkills" rs 
+full join "ApplicantSkills" as2 on as2.name = rs.name
+full join "OpportunitySkills" os on os.name = rs.name
+WHERE coalesce(rs.name, as2.name, os.name) is not null;
