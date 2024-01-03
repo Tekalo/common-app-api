5486e915d7fb2355299524a83f2aef50f047186c
Logan Gittelson <lgittelson@schmidtfutures.com>
2024-01-03 17:21:39 -0500

    Adding timestamp

--- db/hightouch/models/incoming_skills.sql
+++ db/hightouch/models/incoming_skills.sql
@@ -1,8 +1,27 @@
 /**
  * The hard-coded '2023-12-01' references the day the ReferenceSkills were first loaded into the database
  */
-select distinct coalesce(rs.name, as2.name, os.name) as name, COALESCE(LEAST(as2."createdAt", os."createdAt"), '2023-12-01'::date) as "createdAt"
-from "ReferenceSkills" rs 
-full join "ApplicantSkills" as2 on as2.name = rs.name
-full join "OpportunitySkills" os on os.name = rs.name
-WHERE coalesce(rs.name, as2.name, os.name) is not null;
+SELECT
+  COALESCE(rs.name, as2.name, os.name) AS NAME,
+  COALESCE(
+    LEAST(as2."createdAt", os."createdAt"),
+    '2023-12-01T05:00:00' :: TIMESTAMP
+  ) AS "createdAt"
+FROM
+  "ReferenceSkills" rs
+  FULL JOIN "ApplicantSkills" as2 ON as2.name = rs.name
+  FULL JOIN "OpportunitySkills" os ON os.name = COALESCE(rs.name, as2.name)
+WHERE
+  COALESCE(rs.name, as2.name, os.name) IS NOT NULL
+ORDER BY
+  "createdAt" DESC;
+
+
+-- Only run below on first-time load
+SELECT
+    rs.name,
+    rs.name as canonical,
+    '2023-12-01T05:00:00' :: TIMESTAMP as "createdAt",
+    true as suggest
+FROM "ReferenceSkills" rs
+WHERE rs.name is not null;
