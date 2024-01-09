df3280eb25fe6f1addea7be27bcc9e47fb02c05c
Nikki Nikkhoui <nnikkhoui@schmidtfutures.com>
2024-01-09 13:48:21 -0500

    update comment for sql query

--- src/controllers/SkillController.ts
+++ src/controllers/SkillController.ts
@@ -14,8 +14,7 @@ class SkillController {
   }
 
   async getSkills(): Promise<SkillGetResponseBody> {
-    // groupBy to de-duplicate the same canonical skill name regardless of casing
-
+    // De-duplicate the same canonical skill name regardless of casing and return records with priority=true if duplicate records exist
     const skills = await this.prisma
       .$queryRaw`SELECT canonical, sum(cast(priority as int)) > 0 as priority FROM "SkillsView" WHERE suggest = true AND "rejectAs" IS NULL GROUP BY canonical`;
     console.log(skills);
