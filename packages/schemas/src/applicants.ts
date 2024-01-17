1daf64c6fe2c1d4c381baece365de6bd7e3efed5
Nikki Nikkhoui <nnikkhoui@schmidtfutures.com>
2024-01-17 09:41:19 -0500

    remove otherskills

--- packages/schemas/src/applicants.ts
+++ packages/schemas/src/applicants.ts
@@ -163,7 +163,6 @@ const ApplicantSubmissionResponseBody = z.object({
   lastOrg: z.string().max(255).nullable(),
   yoe: z.string().nullable(),
   skills: z.array(z.string()).nullable(),
-  otherSkills: z.array(z.string().max(255)).nullable(),
   linkedInUrl: z.string().max(500).nullable(),
   githubUrl: z.string().max(500).nullable(),
   portfolioUrl: z.string().max(500).nullable(),
