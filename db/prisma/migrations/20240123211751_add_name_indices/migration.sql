cdc3110b747e65c9afeafcbc6d226acd88bc2fb1
Nikki Nikkhoui <nnikkhoui@schmidtfutures.com>
2024-01-23 16:28:23 -0500

    remove unnecessary index

--- db/prisma/migrations/20240123211751_add_name_indices/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateIndex
-CREATE INDEX "ReferenceSkills_name_idx" ON "ReferenceSkills"("name");
-
--- CreateIndex
-CREATE INDEX "SkillsAnnotation_name_idx" ON "SkillsAnnotation"("name");
---
+++ db/prisma/migrations/20240123212756_add_reference_skills_index/migration.sql
@@ -0,0 +1,2 @@
+-- CreateIndex
+CREATE INDEX "ReferenceSkills_name_idx" ON "ReferenceSkills"("name");
